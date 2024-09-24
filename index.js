79c0d53b5d8f40e010658cf6339a2280574e95dc
Wildcode25 <enmanuelcastillo1999@gmail.com>
2024-09-24 16:15:08 -0400

    fix for producction

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import app from './src/servers/express.js'
 import {createServer} from 'http'
 
 const server = createServer(app)
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 server.listen(PORT, ()=>console.log(`server running in ${PORT}`))
