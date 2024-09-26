cac8fb48eda7b155f1f6641b62907d432bf55f16
Wildcode25 <enmanuelcastillo1999@gmail.com>
2024-09-25 21:07:53 -0400

    update ui

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import app from './src/servers/express.js'
 import {createServer} from 'http'
 
 const server = createServer(app)
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 server.listen(PORT, ()=>console.log(`server running in ${PORT}`))
--- src/schemas/list.js
+++ src/schemas/list.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const listSchema = z.object({
-  name: z.string().min(4).max(128),
+  name: z.string().max(128),
   boardId: z.number().int().positive(),
 });
